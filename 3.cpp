4ddfc32a8522ca347de43a4a02bccf178ce4314e
vumbumy <vumbumy@outlook.kr>
2022-05-15 16:43:29 +0900

    Solved 3. Longest Substring Without Repeating Characters

--- 3.cpp
+++ 3.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unordered_set>
+#include <queue>
 #include <string>
 #include <algorithm>
 
@@ -10,15 +11,27 @@ public:
     int lengthOfLongestSubstring(string s) {
         unordered_set<char> c_set;
 
+        queue<char> c_q;
+
         int longest = 0;
         for (char c : s) {
             if (c_set.find(c) != c_set.end()) {
-                c_set.clear();
+                while (true) {
+                    char f = c_q.front();
+
+                    c_q.pop();
+                    c_set.erase(f);
+
+                    if (f == c) {
+                        break;
+                    }
+                }
             }
 
+            c_q.push(c);
             c_set.insert(c);
 
-            int len = static_cast<int>(c_set.size());
+            int len = static_cast<int>(c_q.size());
 
             longest = max(longest, len);
         }
@@ -33,6 +46,8 @@ int main() {
     printf("%d\n", s.lengthOfLongestSubstring("pwwekew"));
     printf("%d\n", s.lengthOfLongestSubstring(""));
     printf("%d\n", s.lengthOfLongestSubstring("dvdf"));
+    printf("%d\n", s.lengthOfLongestSubstring("aaaa"));
+    printf("%d\n", s.lengthOfLongestSubstring("zzzz"));
 
     return 0;
 }
